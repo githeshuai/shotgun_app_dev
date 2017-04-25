15264fb0fa2d92f23bd5d9dacba2d016e453ec05
heshuai <276575758@qq.com>
2017-04-25 17:25:50 +0800

    add app tk-multi-preflight

---
+++ tk-multi-preflight/python/app/dialog.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+import sgtk
+import sys
+sys.path.insert(0, "Z:/miraSG")
+from miraScripts.pipeTools.pipeline.preflight.check_gui import CheckGui
+# by importing QT from sgtk rather than directly, we ensure that
+# the code will be compatible with both PySide and PyQt.
+from sgtk.platform.qt import QtCore, QtGui
+
+
+def show_dialog(app_instance):
+    """
+    Shows the main dialog window.
+    """
+    # in order to handle UIs seamlessly, each toolkit engine has methods for launching
+    # different types of windows. By using these methods, your windows will be correctly
+    # decorated and handled in a consistent fashion by the system. 
+    
+    # we pass the dialog class to this method and leave the actual construction
+    # to be carried out by toolkit.
+    app_instance.engine.show_dialog("preflight...", app_instance, AppDialog)
+    
+
+class AppDialog(QtGui.QWidget):
+    """
+    Main application dialog window
+    """
+    
+    def __init__(self):
+        """
+        Constructor
+        """
+        # first, call the base class and let it do its thing.
+        super(AppDialog, self).__init__()
+
+        main_layout = QtGui.QHBoxLayout(self)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        check_ui = CheckGui()
+        main_layout.addWidget(check_ui)
+        # now load in the UI that was created in the UI designer
+        
+        # most of the useful accessors are available through the Application class instance
+        # it is often handy to keep a reference to this. You can get it via the following method:
+        self._app = sgtk.platform.current_bundle()
+        
+        # via the self._app handle we can for example access:
+        # - The engine, via self._app.engine
+        # - A Shotgun API instance, via self._app.shotgun
+        # - A tk API instance, via self._app.tk 
